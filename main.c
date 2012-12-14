8b69280cb9b953296d57651538c486b70a150e00
Nguyen Le Thanh Dung <nguyentito@gmail.com>
2012-12-14 03:55:59 +0100

    Update.

--- main.c
+++ main.c
@@ -6,13 +6,16 @@
 #include "pn_heuristic.h"
 
 int main(int argc, char *argv[]) {
-    graph_list *g1;
-    graph_list *g2;
-    graph_list *g3;
+    if (argc < 3) {
+        fprintf(stderr, "Usage: heuristitique graph1_file graph2_file\n");
+    }
+
+    graph_list *g1 = NULL;
+    graph_list *g2 = NULL;
 
     FILE *f;
 
-    f = fopen("graph1", "r");
+    f = fopen(argv[1], "r");
     if (f == NULL) {
         fprintf(stderr, "%s : File not found\n", argv[1]);
         exit(1);
@@ -20,34 +23,28 @@ int main(int argc, char *argv[]) {
     g1 = fget_graph_list(f, NULL);
     fclose(f);
 
-    f = fopen("graph2", "r");
+    f = fopen(argv[2], "r");
     if (f == NULL) {
-        fprintf(stderr, "%s : File not found\n", argv[1]);
+        fprintf(stderr, "%s : File not found\n", argv[2]);
         exit(1);
     }
     g2 = fget_graph_list(f, NULL);
     fclose(f);
 
-    f = fopen("graph3", "r");
-    if (f == NULL) {
-        fprintf(stderr, "%s : File not found\n", argv[1]);
-        exit(1);
-    }
-    g3 = fget_graph_list(f, NULL);
-    fclose(f);
-
-    if (!find_isomorphism(g1, g2, NULL))
-        printf("=D 1\n");
-
-    int result[42];
+    int *isom = malloc(g1->n * sizeof(int));
     int i;
-    for (i = 0; i < 42; i++)
-        result[i] = -42;
-    if(find_isomorphism(g1, g1, result))
-        printf("=D 2\n");
-    for (i = 0; i < 42; i++)
-        printf("%d ", result[i]);
-    putchar('\n');
+
+    if (find_isomorphism(g1, g2, isom)) {
+        printf("Here is an isomorphism between the 2 graphs:\n");
+        for (i = 0; i < g1->n; i++) {
+            printf("%d -> %d\n", i, isom[i]);
+        }
+    } else {
+        printf("The 2 graphs are not isomorphic.\n");
+    }
     
+    if (g1 != NULL) gl_free(g1);
+    if (g2 != NULL) gl_free(g2);
+    free(isom);
     return 0;
 }
